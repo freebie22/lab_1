c04a962d1eec91f4aa93637a75b674577837ce0d
freebie22 <bojkov755@gmail.com>
2025-02-03 12:28:03 +0200

    created file structure for tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, Fragment } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Task1 from "./components/Task1";
+import Task2 from "./components/Task2";
+import Task3 from "./components/Task3";
+import Task4 from "./components/Task4";
+import Task5 from "./components/Task5";
+import Task6 from "./components/Task6";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Fragment>
+      <Task1></Task1>
+      <Task2></Task2>
+      <Task3></Task3>
+      <Task4></Task4>
+      <Task5></Task5>
+      <Task6></Task6>
+    </Fragment>
+  );
 }
 
-export default App
+export default App;
